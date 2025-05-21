c32dcc98ddda647cbb009835d9d17fffeb704b6f
Ymirko <Ymirko@DESKTOP-V3P4O0Q>
2025-05-21 14:43:58 +0200

    subiendo

---
+++ src/examen/Main.java
@@ -0,0 +1,33 @@
+package examen;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+        System.out.println("\nIntroduce un número para averiguar si es primo o no: ");
+        
+        int numero = scanner.nextInt();
+        
+        if (esPrimo(numero)) {
+            System.out.println(numero + " es un número primo.");
+        } else {
+            System.out.println(numero + " no es un número primo.");
+        }
+        
+        scanner.close();
+    }
+    
+    public static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
